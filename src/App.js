8598c2a47d1bca92eca1955a83311a58a6e13b78
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2023-08-07 23:58:00 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+// host-app/src/App.js
+import React from "react";
+// import ErrorBoundary from "./ErrorBoundary";
+const RemoteApp = React.lazy(() => import("Remote/App"));
+const RemoteButton = React.lazy(() => import("Remote/Button"));
+ 
+const RemoteWrapper = ({ children }) => (
+ <div
+   style={{
+     border: "1px solid red",
+     background: "white",
+   }}
+ >
+   <div>{children}</div>
+ </div>
+);
+ 
+export const App = () => (
+ <div style={{ background: "rgba(43, 192, 219, 0.3)" }}>
+   <h1>This is the Host!</h1>
+   <h2>Remote App:</h2>
+   <RemoteWrapper>
+     <RemoteApp />
+   </RemoteWrapper>
+   <h2>Remote Button:</h2>
+   <RemoteWrapper>
+     <RemoteButton />
+   </RemoteWrapper>
+   <br />
+   <a href="http://localhost:4000">Link to Remote App</a>
+ </div>
+);
 export default App;
